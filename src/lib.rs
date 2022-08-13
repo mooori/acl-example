7d94e5987a6a6f7dfa2eae47a3ec7a4f4e6a65e3
Moritz <moritz.zielke@gmail.com>
2022-08-13 10:39:12 +0200

    store admins in map (instead of vector)

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,23 @@
 // TODO
 // - add event logs
-// - add init of `acl_*` struct fields in contracts `#[init]` resp. `Default`
-// - disuss: should enumeration be opt-in or opt-out?
+// - discuss: should enumeration be opt-in or opt-out?
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{TreeMap, UnorderedSet, Vector};
+use near_sdk::collections::{TreeMap, UnorderedSet};
 use near_sdk::{env, near_bindgen, AccountId, BorshStorageKey};
-use strum::IntoEnumIterator;
 
 /// Roles are represented by enum variants.
-///
-/// This enum will be used to index a `near_sdk::collections::Vector` which
-/// is indexed by `u64`, hence `repr(u64)`.
-#[derive(Copy, Clone, BorshSerialize, strum::EnumIter)]
-#[repr(u64)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, BorshDeserialize, BorshSerialize)]
 enum Role {
     L1,
     L2,
     L3,
 }
 
-impl Into<u64> for Role {
-    fn into(self) -> u64 {
-        self as u64
-    }
-}
-
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Counter {
     counter: u64,
-
-    // # Motivation for `ACLRoleAdmins` and `ACLRoleGrants`
-    //
-    // Offload as much as possible to separate types, to be less intrusive on
-    // the contract itself and to facilitate testing.
-    //
-    // Separate checks of permission from modification of internal data:
-    // `Contract::grant_role()` just checks permissions and then calls
-    // `ACLRoleGrants::grant_role()` which adds grant to its internal data. This
-    // separation might facilitate testing and maintenance.
-    //
-    // Makes sense?
     acl: Acl<Role>,
 }
 
@@ -51,76 +27,75 @@ impl Counter {
     pub fn new() -> Self {
         let mut acl = Acl::new();
         let caller = env::predecessor_account_id();
-        for role in Role::iter() {
-            acl.add_admin_unchecked(role, &caller);
-        }
+        acl.add_admin_unchecked(Role::L1, &caller);
+        acl.add_admin_unchecked(Role::L2, &caller);
+        acl.add_admin_unchecked(Role::L3, &caller);
 
         Self { counter: 0, acl }
     }
 }
 
+/// Acl manages admins and grants for roles. Roles are represented by variants
+/// of enum `R`.
 // TODO enable opt-out of enumerable collections (efficiency)
 #[derive(BorshDeserialize, BorshSerialize)]
 struct Acl<R> {
-    /// Stores the set of `AccountId`s which are admins for roles (the variants
-    /// of `R`).
-    ///
-    /// Invariant: Let `r` be a variant of `Role`, then
-    /// `admins[Into::<u64>::into(role)]` stores the set of admins for role `r`.
-    ///
-    /// Motivation: without this invariant, `get_admins` would be one of:
-    /// ```
-    /// // Returning an option if `role` is unitialized.
-    /// fn get_admins(&self, role: R) -> Option<UnorderedSet<AccountId>>
-    ///
-    /// // Taking a mutable reference to store a newly initialized set for `role`.
-    /// fn get_admins(&mut self, role: R) -> UnorderesSet<AccountId>
-    /// ```
-    admins: Vector<UnorderedSet<AccountId>>,
-    // TODO get rid of invariant and vector. Instead get_admins_set is private and any modifications of set are not stored.
+    /// Stores the set of `AccountId`s which are admins for each role.
+    admins: TreeMap<R, UnorderedSet<AccountId>>,
+
     /// Stores the set of roles which have been granted to accounts.
+    // TODO use a bit field instead of `UnorderedSet`. To make testing if an
+    // account has at least one of many roles more efficient.
     grants: TreeMap<AccountId, UnorderedSet<R>>,
 }
 
 impl<R> Acl<R>
 where
-    R: Copy + Into<u64> + BorshSerialize,
+    R: Copy + Ord + BorshDeserialize + BorshSerialize,
 {
     pub fn new() -> Self {
-        // Initiate `admins` with an empty set for each variant of `R` to
-        // satisfy the invariant of the `admins` field.
-        let mut admins = Vector::new(ACLStorageKeys::Admins);
-        // TODO avoid enumerate since it might overflow?
-        for (i, role) in Role::iter().enumerate() {
-            // Ensure Role variants are in range [0, num_variants).
-            let i = u64::try_from(i)
-                .unwrap_or_else(|_| env::panic_str("Cannot convert iterator index to u64"));
-            if Into::<u64>::into(role) != i {
-                env::panic_str("Enum cannot be used to index Vector")
-            }
-
-            admins.push(&UnorderedSet::new(ACLStorageKeys::AdminsPerRole {
-                role_hash: role
-                    .try_to_vec()
-                    .unwrap_or_else(|_| env::panic_str("Failed to serialize Role variant")),
-            }))
-        }
-
         Self {
-            admins,
+            admins: TreeMap::new(ACLStorageKeys::Admins),
             grants: TreeMap::new(ACLStorageKeys::Grants),
         }
     }
 
-    /// TODO The returned set may not be a member of the data structure saved to
-    /// storage. If not, changes to the set will not be persisted.
+    fn new_admins_set(role: R) -> UnorderedSet<AccountId> {
+        UnorderedSet::new(ACLStorageKeys::AdminsPerRole {
+            role_hash: role
+                .try_to_vec()
+                .unwrap_or_else(|_| env::panic_str("Failed to serialize Role variant")),
+        })
+    }
+
+    /// For roles that don't have an admin, a newly initialized empty set is
+    /// returned. This set is _not_ a member of the internal data structures and
+    /// it will _not_ be written to storage. In that case, any changes made to
+    /// the returned set will be lost.
     ///
-    /// To retrieve a set that will be saved to storage, use
+    /// However, if there are admins for `role`, the returned set is a member of
+    /// internal storage and modifications will be persisted.
+    ///
+    /// To retrieve a set that will be saved to storage in either case, use
     /// `[get_or_insert_admins_set]`.
     pub fn get_admins_set(&self, role: R) -> UnorderedSet<AccountId> {
-        match self.admins.get(role.into()) {
+        match self.admins.get(&role) {
             Some(set) => set,
-            None => env::panic_str("Invariant violation: missing set of admins for a role"),
+            None => Self::new_admins_set(role),
+        }
+    }
+
+    /// Similar to [`get_admins_set`], but inserting newly initialized sets into
+    /// the data structure written to storage. Use this method if modifications
+    /// to the returned set need to persisted.
+    fn get_or_insert_admins_set(&mut self, role: R) -> UnorderedSet<AccountId> {
+        match self.admins.get(&role) {
+            Some(set) => set,
+            None => {
+                let set = Self::new_admins_set(role);
+                self.admins.insert(&role, &set);
+                set
+            }
         }
     }
 
@@ -141,6 +116,9 @@ where
     /// If the caller is not and admin for `role`, `account_id` is not added to
     /// the set of admins and `None` is returned.
     pub fn add_admin(&mut self, role: R, account_id: &AccountId) -> Option<bool> {
+        // TODO discuss: two lookups happen here: is_admin(), add_admin_unchecked().
+        // What's more important: DRY+readability or micro optimization (avoid methods
+        // to bring the number of lookups down to one)? Same at other places.
         if !self.is_admin(role, &env::predecessor_account_id()) {
             return None;
         }
@@ -150,7 +128,7 @@ where
     /// Adds `account_id` to the set of admins for `role` __without__ checking
     /// if the caller is and admin for `role`.
     pub fn add_admin_unchecked(&mut self, role: R, account_id: &AccountId) -> bool {
-        self.get_admins_set(role).insert(account_id)
+        self.get_or_insert_admins_set(role).insert(account_id)
     }
 
     /// Remove an `account_id` from the set of admins for `role`. If the caller
@@ -163,6 +141,8 @@ where
         if !self.is_admin(role, &env::predecessor_account_id()) {
             return None;
         }
+        // If the set is empty, the modifications here a noops and we don't mind
+        // _not_ persisting them. So no need for `get_or_insert_admins_set`.
         let mut set = self.get_admins_set(role);
         Some(set.remove(account_id))
     }
@@ -170,6 +150,8 @@ where
     /// Removes the calling account from the set of admins for `role`. Returns
     /// whether the caller was an admin for `role`.
     fn renounce_admin(&mut self, role: R) -> bool {
+        // If the set is empty, the modifications here a noops and we don't mind
+        // _not_ persisting them. So no need for `get_or_insert_admins_set`.
         self.get_admins_set(role)
             .remove(&env::predecessor_account_id())
     }
